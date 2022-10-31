7c818eabce8688b184f3ca29879e48093bda5532
Von-Matterhorn <matteomengarelli@libero.it>
2022-10-31 17:01:30 +0100

    Week 2 day 1

---
+++ m1/w2/d1/Matteo Mengarelli esercizio w2 d1/js/index.js
@@ -0,0 +1,14 @@
+alert('I numeri funzionanti sono 20, 63, 93, 23 e 43')
+var pilota  = prompt('Scrivi il numero del tuo pilota preferito')
+console.log(pilota)
+if(pilota == 20){
+    document.write('<p class="pilota">Il tuo pilota preferito è Fabio Quartararo</p>')
+}
+else if(pilota == 63){
+    document.write('<p class="pilota">Il tuo pilota preferito è Francesco Bagnaia</p>')}
+else if(pilota == 93){
+    document.write('<p class="pilota">Il tuo pilota preferito è Marc Marquez</p>')}
+else if(pilota == 23){
+    document.write('<p class="pilota">Il tuo pilota preferito è Enea Bastianini</p>')}
+else if(pilota == 43){
+    document.write('<p class="pilota">Il tuo pilota preferito è Jack Miller</p>')}
