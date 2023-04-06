2d864ed6f61cb498b05534d08724973e3e96b723
VonMatterhorn <matteomengarelli@libero.it>
2023-04-06 17:14:19 +0200

    project

---
+++ project/angular/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FilteredComponent } from './filtered/filtered.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    GameComponent,
+    ProfileComponent,
+    FilteredComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
